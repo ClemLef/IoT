ca3e88b48bbbac008455f05451b50b19f2f12681
ClemLef <clement.lef@neuf.fr>
2022-12-20 17:04:06 +0100

    implemeted multithreading

--- Lab2/Mqtt.cpp
+++ Lab2/Mqtt.cpp
@@ -16,8 +16,14 @@ using namespace std;
 
 #define PORT 1883
 
-pthread_t thread_tcp [100], thread_file [10];
-int threadno_tcp = 0;
+typedef struct
+{
+	int sock;
+	struct sockaddr address;
+	socklen_t addr_len;
+} connection_t;
+
+
 
 string connectAck() {
 	string message = "";
@@ -76,82 +82,164 @@ void getControlPacketType(char* buffer, unsigned char* controlPacketType, int ms
 	//cout << endl;
 } 
 
-void getTopic(char* buffer, unsigned char* topic, unsigned char* controlPacketType){
-	int topicLength = controlPacketType[3];
+void getTopic(char* buffer, unsigned char* topic){
+	int topicLength = buffer[3];
 	//print topic 
 	cout << "topic : ";
 	for(int i = 4; i < topicLength + 4; i++){
-		topic[i] = controlPacketType[i];
+		topic[i] = buffer[i];
 		cout << topic[i];
 	}
 	cout << endl;
 }
 
-void getMessage(char* buffer, unsigned char* message, unsigned char* controlPacketType){
-	int topicLength = controlPacketType[3];
+void getMessage(char* buffer, unsigned char* message){
+	int topicLength = buffer[3];
 	//print msg
 	cout << "message : ";
 	for(int i = topicLength + 4; i < 2 + buffer[1]; i++){
-		message[i] = controlPacketType[i];
+		message[i] = buffer[i];
 		cout << message[i];
 	}
 	cout << endl;
 }
 
-int main(int argc, char const* argv[])
+void * process(void * ptr)
 {
-	int server_fd, new_socket, valread;
-	struct sockaddr_in address;
-	int opt = 1;
-	int addrlen = sizeof(address);
-	char buffer[1024] = { 0 };
-	unsigned char controlPacketType[100] = {0};
+    char buffer[1024] = {0};
+    connection_t * conn;
+    long addr = 0;
 	string response;
+	unsigned char controlPacketType;
 	unsigned char packetID[2] = {0};
 
 	unsigned char topic[100] = {0};
 	unsigned char message[1024] = {0};
 
-	// Creating socket file descriptor
-	if ((server_fd = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
-		perror("socket failed");
-		exit(EXIT_FAILURE);
-	}
+	if (!ptr) pthread_exit(0);
+    conn = (connection_t *)ptr;
 
-	// Forcefully attaching socket to the port 8080
-	if (setsockopt(server_fd, SOL_SOCKET,
-				SO_REUSEADDR | SO_REUSEPORT, &opt,
-				sizeof(opt))) {
-		perror("setsockopt");
-		exit(EXIT_FAILURE);
+	cout << hex << (int)buffer[0] << endl;
+	while (buffer[0] != 0xe0){
+		//addr = (long)((struct sockaddr_in *)&conn->address)->sin_addr.s_addr;
+		//buffer = (char *)malloc((len+1)*sizeof(char));
+		//buffer[len] = 0;
+		/* read message */
+		read(conn->sock, buffer, 1024);
+		/* print message */
+		/*printf("%d.%d.%d.%d:\n",
+			(addr      ) & 0xff,
+			(addr >>  8) & 0xff,
+			(addr >> 16) & 0xff,
+			(addr >> 24) & 0xff,
+			);*/
+		//cout << len << endl;
+		for(int i = 0; i < 200; i++){
+			cout << hex << (int)buffer[i];
+		}
+		cout << endl;
+		cout << hex << (int)buffer[0] << endl;
+		controlPacketType = buffer[0];
+		switch (controlPacketType)
+		{
+		//connect
+		case 0x10:
+			response = connectAck();
+			send(conn->sock, response.c_str(), response.length(), 0);
+			break;
+		//ping
+		case 0xc0:
+			response = pingAck();
+			send(conn->sock, response.c_str(), response.length(), 0);
+			break;
+		//subscribe
+		case 0x82:
+			packetID[0] = buffer[2];
+			packetID[1] = buffer[3];
+			response = subscribeAck(packetID);
+			send(conn->sock, response.c_str(), response.length(), 0);
+			break;
+		//unsubscribe
+		case 0xa2:
+			packetID[0] = buffer[2];
+			packetID[1] = buffer[3];
+			response = unsubscribeAck(packetID);
+			send(conn->sock, response.c_str(), response.length(), 0);
+			break;
+		//publish
+		case 0x30:
+			getTopic(buffer, topic);
+			getMessage(buffer, message);
+			//Todo : add the message to the topic list and send it to the other clients
+			break;
+		//disconnect
+		case 0xe0:
+			close(conn->sock);
+			break;
+		default:
+			break;
+		}
 	}
-	address.sin_family = AF_INET;
-	address.sin_addr.s_addr = INADDR_ANY;
-	address.sin_port = htons(PORT);
+	/* close socket and clean up */
+	free(buffer);
+    close(conn->sock);
+    free(conn);
+    pthread_exit(0);
+}
 
-	// Forcefully attaching socket to the port 8080
-	if (bind(server_fd, (struct sockaddr*)&address,
-			sizeof(address))
-		< 0) {
-		perror("bind failed");
-		exit(EXIT_FAILURE);
-	}
-	if (listen(server_fd, 3) < 0) {
-		perror("listen");
-		exit(EXIT_FAILURE);
-	}
+int main(int argc, char const* argv[])
+{
+	int sock = -1;
+    struct sockaddr_in address;
+    connection_t * connection;
+    pthread_t thread;
 
-	if ((new_socket
-			= accept(server_fd, (struct sockaddr*)&address,
-					(socklen_t*)&addrlen))
-			< 0) {
-			perror("accept");
-			exit(EXIT_FAILURE);
-		} 
+	/* create socket */
+    sock = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
+    if (sock <= 0)
+    {
+        fprintf(stderr, "%s: error: cannot create socket\n", argv[0]);
+        return -3;
+    }
+
+	/* bind socket to port */
+    address.sin_family = AF_INET;
+    address.sin_addr.s_addr = INADDR_ANY;
+    address.sin_port = htons(PORT);
+    if (bind(sock, (struct sockaddr *)&address, sizeof(struct sockaddr_in)) < 0)
+    {
+        fprintf(stderr, "%s: error: cannot bind socket to port %d\n", argv[0], PORT);
+        return -4;
+    }
+
+	/* listen on port */
+    if (listen(sock, 5) < 0)
+    {
+        fprintf(stderr, "%s: error: cannot listen on port\n", argv[0]);
+        return -5;
+    }
+
+	printf("%s: ready and listening\n", argv[0]);
 
     while (1){
 
-        valread = read(new_socket, buffer, 1024);
+		/* accept incoming connections */
+        connection = (connection_t *)malloc(sizeof(connection_t));
+        connection->sock = accept(sock, &connection->address, &connection->addr_len);
+        if (connection->sock <= 0)
+        {
+            free(connection);
+        }
+        else
+        {
+            /* start a new thread but do not wait for it */
+            pthread_create(&thread, 0, process, (void *)connection);
+            pthread_detach(thread);
+        }
+
+
+
+        /*valread = read(new_socket, buffer, 1024);
 		getControlPacketType(buffer, controlPacketType, 2 + buffer[1]);
 		cout << hex << (int)controlPacketType[0] << endl;
 		
@@ -195,7 +283,7 @@ int main(int argc, char const* argv[])
 			break;
 		default:
 			break;
-		}	
+		}	*/
     }
 	return 0;
 }
