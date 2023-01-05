d12e681c300cf8ac86c8a5a95f6ae33aaa6a421b
ClemLef <clement.lef@neuf.fr>
2023-01-05 12:39:23 +0100

    Client that sends sensor data to Mqtt broker

--- Lab3/MqttSensorClient.cpp
+++ Lab3/MqttSensorClient.cpp
@@ -1,14 +1,44 @@
-#include <fstream> 
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+#include <MQTTClient.h>
 
-int readTemp(){
-	string tempStr;
-	float temp;
-	// Read from the file
-	ifstream MyReadFile("/sys/class/thermal/thermal_zone0/temp");
-	getline (MyReadFile, tempStr);
-	cout << tempStr;
-	temp = stof(tempStr);
-	temp = temp/1000;
-	cout << temp;
-	return temp;
+using namespace std;
+
+const char *ADDRESS = "tcp://192.168.137.64";
+const char *TOPIC = "testtopic/hello";
+
+int main(int argc, char* argv[]) {
+    const char *MESSAGE_STRING = "Hello, this is a message from the RevPi";
+
+    MQTTClient mqttClient;
+    MQTTClient_connectOptions connectionOptions =     MQTTClient_connectOptions_initializer;
+    MQTTClient_message message = MQTTClient_message_initializer;
+    MQTTClient_deliveryToken deliveryToken;
+
+    MQTTClient_create(&mqttClient, ADDRESS, "RevPi", 
+    MQTTCLIENT_PERSISTENCE_NONE, NULL);
+
+    connectionOptions.keepAliveInterval = 20;
+    connectionOptions.cleansession = 1;
+
+    if (MQTTClient_connect(mqttClient, &connectionOptions) != MQTTCLIENT_SUCCESS) {
+        printf("Failed to connect\n");
+        return (-1);
+    }
+
+     // Copy the message string to avoid compiler warnings
+    char *s = new char[strlen(MESSAGE_STRING)+1];
+    strcpy(s,MESSAGE_STRING);
+
+    message.payload = s;
+    message.payloadlen = strlen(s);
+    message.qos = 0;
+    message.retained = 0;
+    
+    MQTTClient_publishMessage(mqttClient, TOPIC, &message, &deliveryToken);
+    MQTTClient_disconnect(mqttClient, 5000);
+    MQTTClient_destroy(&mqttClient);
+
+    return (0);
 }
